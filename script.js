839af831cc3083d21fcbaecde2bf536dc7db84f8
hodu0118 <taeorange79@gmail.com>
2026-03-11 21:16:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,115 @@
 let feed = document.getElementById("feed")
 
+let posts = JSON.parse(localStorage.getItem("posts")) || []
+
+renderPosts()
+
 function createPost(){
 
 let username = document.getElementById("username").value
 let title = document.getElementById("title").value
-let image = document.getElementById("image").value
 let desc = document.getElementById("desc").value
+let tag = document.getElementById("tag").value
+let file = document.getElementById("imageFile").files[0]
 
-let post = document.createElement("div")
-post.className = "post"
+let reader = new FileReader()
 
-post.innerHTML = `
-<img src="${image}">
-<h3>${title}</h3>
-<p>${desc}</p>
-<p>👤 ${username}</p>
-<p class="likes" onclick="like(this)">❤️ 0 Likes</p>
-`
+reader.onload = function(e){
 
-feed.prepend(post)
+let post = {
+
+username,
+title,
+desc,
+tag,
+image:e.target.result,
+likes:0,
+comments:[]
 
 }
 
-function like(el){
+posts.unshift(post)
 
-let num = parseInt(el.innerText.split(" ")[1])
-num++
+save()
 
-el.innerText = "❤️ " + num + " Likes"
+renderPosts()
+
+}
+
+if(file){
+reader.readAsDataURL(file)
+}
+
+}
+
+function renderPosts(){
+
+feed.innerHTML=""
+
+posts.forEach((post,index)=>{
+
+let div = document.createElement("div")
+div.className="post"
+
+div.innerHTML=`
+
+<img src="${post.image}">
+<p><b>${post.title}</b></p>
+
+<span class="tag">${post.tag}</span>
+
+<p>${post.desc}</p>
+
+<p>👤 ${post.username}</p>
+
+<p class="likes" onclick="likePost(${index})">❤️ ${post.likes}</p>
+
+<div id="comments-${index}">
+${post.comments.map(c=>`<div class="comment">💬 ${c}</div>`).join("")}
+</div>
+
+<input class="commentInput" placeholder="댓글..." onkeypress="commentEnter(event,${index},this)">
+
+`
+
+feed.appendChild(div)
+
+})
+
+}
+
+function likePost(index){
+
+posts[index].likes++
+
+save()
+
+renderPosts()
+
+}
+
+function commentEnter(event,index,input){
+
+if(event.key==="Enter"){
+
+let text=input.value.trim()
+
+if(!text) return
+
+posts[index].comments.push(text)
+
+input.value=""
+
+save()
+
+renderPosts()
+
+}
+
+}
+
+function save(){
+
+localStorage.setItem("posts",JSON.stringify(posts))
 
 }
